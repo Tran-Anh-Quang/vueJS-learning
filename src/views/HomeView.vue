03355f2131abe7ecd9575ba24e26e20872798c27
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-03-05 08:39:16 +0700

    render a list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,10 +8,36 @@ const handleIncrease = (data) => {
   count.value = count.value + data.value;
 }
 
+// sử dụng ref cho array còn 1 object bên trong là array thì dùng reactive
+const products = ref([
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300
+  }
+])
+
 </script>
 
 <template>
   <main>
     <TheWelcome :count="count" @handle-increase="handleIncrease"/>
+
+    <div v-for="product in products">
+      <div class="group-product-item">
+        <div>{{ product.name }}</div>
+        <div>{{ product.price }}</div>
+      </div>
+    </div>
   </main>
 </template>
